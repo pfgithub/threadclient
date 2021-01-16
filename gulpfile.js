6fa0fb47638b0ccf7c55b8d76c06c9fd97836337
pfg <pfg@pfg.pw>
2021-01-16 10:30:54 -0800

    scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp");
 const sourcemaps = require("gulp-sourcemaps");
 const babel = require("gulp-babel");
+const sass = require("gulp-sass");
 const concat = require("gulp-concat");
 const fs = require("fs");
 
 const typescript = function() {
-    return gulp
-        .src("src/**/*.ts")
+    return gulp.src("src/**/*.ts")
         .pipe(sourcemaps.init())
         .pipe(babel({
             "presets": [
@@ -23,16 +23,24 @@ const typescript = function() {
             ],
         }))
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest("dist"))
+    ;
+}
+const scss = function() {
+    return gulp.src("src/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("dist"))
+    ;
 }
 const nocomp = function() {
     return gulp.src("static/**/*").pipe(gulp.dest("dist"));
 }
 
-const all = gulp.series(typescript, nocomp);
+const all = gulp.series(typescript, scss, nocomp);
 gulp.task("all", all);
 
 gulp.task("watch", gulp.series("all", () => {
     gulp.watch("src/**/*.ts", typescript);
+    gulp.watch("src/**/*.scss", scss);
     gulp.watch("static/**/*", nocomp);
 }));
