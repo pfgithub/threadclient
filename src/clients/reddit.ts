ec4b91ecad7d37d82a8776c884fe7fc9f6886ccf
pfg <pfg@pfg.pw>
2021-02-03 23:39:27 -0800

    add another thing. no idea what it does but whatever.

--- src/clients/reddit.ts
+++ src/clients/reddit.ts
@@ -265,7 +265,8 @@ const pathURL = (path: string) => {
     }
     query.set("raw_json", "1");
     query.set("rtj", "yes"); // undefined | "yes" | "only" but it turns out in listings eg /r/subreddit.json rtj=only cuts off after like 10 paragraphs but rtj=yes doesn't weird
-    query.set("emotes_as_images", "true");
+    query.set("emotes_as_images", "true"); // enables sending {t: "gif"} span elements in richtext rather than sending a link
+    query.set("gilding_detail", "1"); // not sure what this does but new.reddit sends it in an oauth.reddit.com request so it sounds good
     return baseURL()+pathname+".json?"+query.toString();
 };
 
