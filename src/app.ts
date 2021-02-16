9fed01d209d8cc2b02cad93cba9fca5c5258e4e9
pfg <pfg@pfg.pw>
2021-02-16 01:28:42 -0800

    clips.twitch.tv preview

--- src/app.ts
+++ src/app.ts
@@ -404,6 +404,135 @@ function canPreview(client: ThreadClient, link: string, opts: {autoplay: boolean
             return hsc;
         };
     }
+    if(url && url.host === "clips.twitch.tv" && url.pathname.split("/").length === 2) {
+        const clipid = url.pathname.split("/")[1];
+        return (): HideShowCleanup<Node> => {
+            const frame = el("div");
+            const hsc = hideshow(frame);
+
+            const loading = loadingSpinner().adto(frame);
+
+            function gqlRequest(operation: string, hash: string, variables: unknown) {
+                return {
+                    extensions: {persistedQuery: {sha256Hash: hash, version: 1}},
+                    operationName: operation,
+                    variables,
+                };
+            }
+
+            fetch("https://gql.twitch.tv/gql", {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json",
+                    'Accept': "application/json",
+                    'Client-Id': "kimne78kx3ncx6brgo4mv6wki5h1ko",
+                },
+                body: JSON.stringify([
+                    gqlRequest("VideoAccessToken_Clip",
+                        "36b89d2507fce29e5ca551df756d27c1cfe079e2609642b4390aa4c35796eb11",
+                        {slug: clipid}
+                    ),
+                    gqlRequest("ClipsChatCard",
+                        "94c1c7d97d860722a5b7ef3c3b3de3783b37fc32d69bcccc8ea0cda372cf1f01",
+                        {slug: clipid}
+                    ),
+                    gqlRequest("ClipsBroadcasterInfo", // 9
+                        "ce258d9536360736605b42db697b3636e750fdb14ff0a7da8c7225bdc2c07e8a",
+                        {slug: clipid}
+                    ),
+                    gqlRequest("ClipsTitle", // 12
+                        "f6cca7f2fdfbfc2cecea0c88452500dae569191e58a265f97711f8f2a838f5b4",
+                        {slug: clipid}
+                    ),
+                    gqlRequest("ClipsCurator", // 13
+                        "769e99d9ac3f68e53c63dd902807cc9fbea63dace36c81643d776bcb120902e2",
+                        {slug: clipid}
+                    ),
+                    gqlRequest("ClipsFullVideoButton", // 14
+                        "d519a5a70419d97a3523be18fe6be81eeb93429e0a41c3baa9441fc3b1dffebf",
+                        {slug: clipid}
+                    ),
+                ]),
+            }).then(r => r.json()).then(res_untyped => {
+                loading.remove();
+                console.log(res_untyped);
+                const [video, chat_card, broadcaster_info, title, curator, full_video_btn] = res_untyped as [
+                    // wow lots of redundant data
+                    video: {data: {clip: {
+                        id: string,
+                        playbackAccessToken: {
+                            signature: string,
+                            value: string, // JSON {authorization: {forbidden: false, reason: ""}, clip_url: "", device_id: null, expires: number, user_id: "", version: 2}
+                        },
+                        videoQualities: {
+                            frameRate: number,
+                            quality: "360" | "480" | "720" | "unsupported",
+                            sourceURL: string,
+                        }[],
+                    }}},
+                    chat_card: {data: {clip: {
+                        id: string,
+                        videoOffsetSeconds: number,
+                        createdAt: string,
+                        curator: {id: string, login: string},
+                        video: {id: string, login: string},
+                    }}},
+                    broadcaster_info: {data: {clip: {
+                        id: string,
+                        game: {name: string, displayName: string},
+                        broadcaster: {
+                            id: string,
+                            profileImageURL: string,
+                            displayName: string,
+                            login: string,
+                            stream: null,
+                        },
+                    }}},
+                    title: {data: {clip: {
+                        id: string,
+                        title: string,
+                    }}},
+                    curator: {data: {clip: {
+                        id: string,
+                        curator: {id: string, displayName: string, login: string},
+                    }}},
+                    full_video_btn: {data: {clip: {
+                        id: string,
+                        videoOffsetSeconds: number,
+                        durationSeconds: number,
+                        title: string,
+                        broadcaster: {id: string, login: string},
+                        video: {id: string, broadcastType: "ARCHIVE" | "unsupported"},
+                        game: {id: string, displayName: string},
+                    }}},
+                ];
+                el("h1").clss("text-xl font-bold text-gray-500").adto(frame).atxt(title.data.clip.title);
+                videoPreview(video.data.clip.videoQualities.map(quality => {
+                    return {src: quality.sourceURL};
+                }), {autoplay: opts.autoplay, gifv: false}).defer(hsc).adto(frame);
+
+                () => [chat_card, broadcaster_info, curator, full_video_btn]; 
+
+                // TODO ClipsChatReplay 05bb2716e4760d4c5fc03111a5afe9b0ab69fc875e9b65ea8a63bbc34d5af21d
+                // variables:
+                // - slug,
+                // - videoOffsetSeconds,
+                // → something that gives a cursor
+                // then
+                // ClipsChatReplay 05bb2716e4760d4c5fc03111a5afe9b0ab69fc875e9b65ea8a63bbc34d5af21d
+                // variables:
+                // - slug,
+                // - cursor,
+                // → the chat messages
+            }).catch(e => {
+                console.log(e);
+                try {loading.remove()}catch(er){()=>er}
+                el("div").clss("error").adto(frame).atxt(e.toString());
+            });
+
+            return hsc;
+        };
+    }
     if(opts.suggested_embed != null) return (): HideShowCleanup<Node> => {
         // TODO: render a body with markdown type unsafe-html that supports iframes
         try {
