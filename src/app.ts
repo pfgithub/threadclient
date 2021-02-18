3a2fb190456fe78597b1b7c84c02d868287ca5ff
pfg <pfg@pfg.pw>
2021-02-18 02:52:15 -0800

    github link

--- src/app.ts
+++ src/app.ts
@@ -2834,10 +2834,14 @@ function homePage(): HideShowCleanup<HTMLDivElement> {
         <div class="mt-10"></div>
         <p>Try for <span id="reddit"></span></p>
         <p>Try for <span id="mastodon"></span></p>
+        <div class="mt-10"></div>
+        <p>Threadreader is open source on <span id="github"></span> and licensed under the <span id="license"></span>.</p>
       </div>
     </div>`;
     res.querySelector("#reddit")!.appendChild(linkButton("", "/reddit", "normal").atxt("Reddit").adto(res));
     res.querySelector("#mastodon")!.appendChild(linkButton("", "/mastodon", "normal").atxt("Mastodon").adto(res));
+    res.querySelector("#github")!.appendChild(linkButton("", "https://github.com/pfgithub/threadclient", "normal").atxt("Github").adto(res));
+    res.querySelector("#license")!.appendChild(linkButton("", "https://github.com/pfgithub/threadclient/blob/main/LICENSE", "normal").atxt("License").adto(res));
 
     return hsc;
 }
