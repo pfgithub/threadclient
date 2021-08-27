af67fd0e36d71dcc7bd0f9c235e93940046dee4d
pfg <pfg@pfg.pw>
2021-08-27 11:33:32 -0700

    more drawing updates

--- packages/animator-ui-web/src/animator.tsx
+++ packages/animator-ui-web/src/animator.tsx
@@ -36,19 +36,77 @@ function AnimationCanvas(): JSX.Element {
         getStroke(points()) as [number, number][]
     ));
 
+    const [transform, setTransform] = createSignal(new DOMMatrixReadOnly());
+
+    const onpointerdown = (e: PointerEvent) => {
+        e.preventDefault();
+        setDrawing(true);
+        setPoints([
+            eventToPoint(e),
+        ]);
+    };
+    document.addEventListener("pointerdown", onpointerdown);
+    onCleanup(() => document.removeEventListener("pointerdown", onpointerdown));
+
+    const eventToPoint = (e: PointerEvent): Point => {
+        const point = transform().transformPoint(new DOMPoint(e.pageX, e.pageY));
+        return [point.x, point.y, e.pressure];
+    };
+
+    const onpointermove = (e: PointerEvent) => {
+        if(drawing()) {
+            e.preventDefault();
+            setPoints(pts => [
+                ...pts,
+                eventToPoint(e),
+            ]);
+        }
+    };
+    document.addEventListener("pointermove", onpointermove);
+    onCleanup(() => document.removeEventListener("pointermove", onpointermove));
+
+    const onpointerup = (e: PointerEvent) => {
+        e.preventDefault();
+        setDrawing(false);
+        setPoints(pts => [
+            ...pts,
+            eventToPoint(e),
+        ]);
+    };
+    document.addEventListener("pointerup", onpointerup);
+    onCleanup(() => document.removeEventListener("pointerup", onpointerup));
+
+    const onpointercancel = (e: PointerEvent) => {
+        setDrawing(false);
+        setPoints([]);
+    };
+    document.addEventListener("pointercancel", onpointercancel);
+    onCleanup(() => document.removeEventListener("pointercancel", onpointercancel));
+
+    const onkeypress = (e: KeyboardEvent) => {
+        if(e.key === "a") {
+            setTransform(t => t.rotate(0.2));
+        }
+    };
+    document.addEventListener("keypress", onkeypress);
+    onCleanup(() => document.removeEventListener("keypress", onkeypress));
+
+    // const draw_size: [w: number, h: number] = [1920, 1080];
+
     return <canvas ref={canvas => {
         const ctx = canvas.getContext("2d")!;
 
         createEffect(() => {
-            canvas.width = pageSize().width;
-            canvas.height = pageSize().height;
-            // redraw
-        });
-
-        createEffect(() => {
             const point_s = stroke();
             const page_size = pageSize();
+            const pr = window.devicePixelRatio;
+            if(canvas.width !== page_size.width * pr) canvas.width = page_size.width * pr;
+            if(canvas.height !== page_size.height * pr) canvas.height = page_size.height * pr;
+            ctx.setTransform(new DOMMatrixReadOnly().scale(pr));
             ctx?.clearRect(0, 0, page_size.width, page_size.height);
+
+            ctx.setTransform(transform().inverse().scale(pr));
+
             ctx.beginPath();
             let zero = true;
             for(const [x, y] of point_s) {
@@ -63,34 +121,6 @@ function AnimationCanvas(): JSX.Element {
         });
     }}
         class="w-full h-full"
-        onpointerdown={e => {
-            e.preventDefault();
-            setDrawing(true);
-            setPoints([
-                [e.pageX, e.pageY, e.pressure],
-            ]);
-        }}
-        onpointermove={e => {
-            if(drawing()) {
-                e.preventDefault();
-                setPoints(pts => [
-                    ...pts,
-                    [e.pageX, e.pageY, e.pressure],
-                ]);
-            }
-        }}
-        onpointerup={e => {
-            e.preventDefault();
-            setDrawing(false);
-            setPoints(pts => [
-                ...pts,
-                [e.pageX, e.pageY, e.pressure],
-            ]);
-        }}
-        onpointercancel={e => {
-            setDrawing(false);
-            setPoints([]);
-        }}
     />;
 }
 
