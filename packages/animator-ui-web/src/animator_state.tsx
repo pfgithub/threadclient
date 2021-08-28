a1751eb70a96cd7895de727d6e88e6e4790092d5
pfg <pfg@pfg.pw>
2021-08-28 16:22:56 -0700

    ios support for the animator

--- packages/animator-ui-web/src/animator_state.tsx
+++ packages/animator-ui-web/src/animator_state.tsx
@@ -1,6 +1,6 @@
 import { batch, createSignal, JSX } from "solid-js";
 import { createStore, reconcile } from "solid-js/store";
-import { ShowCond } from "tmeta-util-solid";
+import { ShowBool, ShowCond } from "tmeta-util-solid";
 import { switchKind } from "../../tmeta-util/src/util";
 import Animator from "./animator";
 import {
@@ -83,6 +83,7 @@ export function AnimatorStateHolder(props: {
 
 export default function AnimatorState(): JSX.Element {
     const [data, setData] = createSignal<null | {config: Config, audio: AudioBuffer}>(null);
+    const [tested, setTested] = createSignal(false);
 
     const audio_ctx = new AudioContext();
 
@@ -104,11 +105,44 @@ export default function AnimatorState(): JSX.Element {
     });
 
     return <ShowCond when={data()} fallback={
-        <div class="h-full flex flex-col flex-wrap items-center justify-center">
-            Loading…
+        <div class="h-full flex flex-col flex-wrap items-center justify-center bg-gray-100">
+            <svg
+                class="animate-spin mr-1 h-5 w-5 text-black inline-block align-middle"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <title>Loading…</title>
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d={"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 "
+                    +"5.824 3 7.938l3-2.647z"}
+                ></path>
+            </svg>
         </div>
     }>{content => (
-        <AnimatorStateHolder config={content.config} audio={content.audio} ctx={audio_ctx} />
+        <ShowBool when={tested()} fallback={
+            <button
+                class="w-full h-full flex flex-col flex-wrap items-center justify-center bg-gray-100"
+            onClick={() => {
+                const source = audio_ctx.createBufferSource();
+                source.buffer = content.audio;
+                source.connect(audio_ctx.destination);
+                source.start(0, 0, 1);
+                setTested(true);
+                source.addEventListener("ended", () => {
+                    setTested(true);
+                });
+            }}>
+                <span
+                    class="border px-4 py-2 shadow-md bg-white transform hover:scale-110 hover:shadow-lg transition"
+                >Start</span>
+            </button>
+        }>
+            <AnimatorStateHolder config={content.config} audio={content.audio} ctx={audio_ctx} />
+        </ShowBool>
     )}</ShowCond>;
 }
 
