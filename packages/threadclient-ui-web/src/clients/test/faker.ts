1f9aba1ebc6bdbcc29625b962a6d46173802ba8e
pfg <pfg@pfg.pw>
2022-01-27 19:31:40 -0500

    more faker content

--- packages/threadclient-ui-web/src/clients/test/faker.ts
+++ packages/threadclient-ui-web/src/clients/test/faker.ts
@@ -1,13 +1,8 @@
 import faker from "@faker-js/faker";
-
 import type * as Generic from "api-types-generic";
-import { Richtext, rt } from "api-types-generic";
-import * as commonmark from "commonmark";
-import { encoderGenerator, ThreadClient } from "threadclient-client-base";
+import { rt } from "api-types-generic";
 import { assertNever } from "tmeta-util";
-import { LogEntry, variables } from "virtual:_variables";
 
-() => faker;
 
 // ok
 // ids will be like this:
@@ -53,10 +48,108 @@ function getReplies(id_link: Generic.Link<Generic.Post>): Generic.Link<Generic.P
     const id = id_link.toString();
     faker.seed(setSeed(id, "getReplies"));
 
+    // subreddits will be paginated and go on forever
+    // posts will have 0-50 replies
+    // comments will have 0-3 replies
+
+    const kind = postKind(id);
+    if(kind === "comment") {
+        const count = faker.datatype.number(3);
+        return new Array(count).fill(0).map(() => {
+            return newLink<Generic.Post>(id + "/" + faker.random.alphaNumeric(6));
+        });
+    }
+
     const count = faker.datatype.number(50);
     return new Array(count).fill(0).map(() => {
         return newLink<Generic.Post>(id + "/" + faker.random.alphaNumeric(6));
     });
+}
+
+function generateTextBody(): Generic.Body {
+    return {
+        kind: "richtext",
+        content: new Array(3).fill(0).map(() => rt.p(rt.txt(faker.lorem.paragraph()))),
+    };
+}
+
+function generateThumbnailFor(body: Generic.Body): Generic.Thumbnail {
+    if(body.kind === "captioned_image") {
+        return {
+            kind: "image",
+            url: body.url.replace(/\/\d+?\/\d+?.jpg/, "/140/140.jpg"),
+        };
+    }
+    if(body.kind === "richtext") {
+        return {
+            kind: "default",
+            thumb: "self",
+        };
+    }
+    return {
+        kind: "default",
+        thumb: "default",
+    };
+}
+
+function generatePostBody(): Generic.Body {
+    const pick = faker.random.arrayElement(["text", "image"]);
+    if(pick === "text") {
+        return generateTextBody();
+    }else{
+        const w = 650;
+        const h = 365;
+        return {
+            kind: "captioned_image",
+            url: generatePostImage(w, h),
+            w: w,
+            h: h,
+        };
+    }
+}
+
+function generatePost(): Generic.PostContent {
+    // consider some kind of pushSeed() defer popSeed() thing
+    // so we don't have to deal with weird global state
+
+    const body = generatePostBody();
+
+    return {
+        kind: "post",
+        title: {text: faker.lorem.sentence()},
+        body,
+        author: generateAuthor(),
+        show_replies_when_below_pivot: false,
+        collapsible: {default_collapsed: true},
+        thumbnail: generateThumbnailFor(body),
+    };
+}
+
+function generateComment(): Generic.PostContent {
+    return {
+        kind: "post",
+        title: null,
+        body: generateTextBody(),
+        author: generateAuthor(),
+        show_replies_when_below_pivot: true,
+        collapsible: {default_collapsed: false},
+    }
+}
+
+function postKind(id: string): "post" | "comment" {
+    if(id.split("/").length > 3) return "comment";
+    return "post";
+}
+
+function generateContent(id: string): Generic.PostContent {
+    const kind = postKind(id);
+    if(kind === "post") {
+        return generatePost();
+    }else if(kind === "comment") {
+        return generateComment();
+    }else{
+        assertNever(kind);
+    }
 }
 
 function generate(content: Generic.Page2Content, id_link: Generic.Link<Generic.Post>): void {
@@ -73,29 +166,15 @@ function generate(content: Generic.Page2Content, id_link: Generic.Link<Generic.P
         },
 
         kind: "post",
-        content: {
-            kind: "post",
-            title: {text: faker.lorem.sentence()},
-            body: {
-                kind: "richtext",
-                content: new Array(3).fill(0).map(() => rt.p(rt.txt(faker.lorem.paragraph()))),
-            },
-            author: generateAuthor(),
-            show_replies_when_below_pivot: false,
-            collapsible: {default_collapsed: true},
-            thumbnail: {
-                kind: "image",
-                url: generateThumbnailImage(),
-            },
-        },
+        content: generateContent(id),
         internal_data: id,
         display_style: "centered",
     });
 }
 
-function generateThumbnailImage() {
+function generatePostImage(w: number, h: number) {
     const seed = faker.random.alphaNumeric(6);
-    return "https://picsum.photos/seed/"+seed+"/140/140";
+    return "https://picsum.photos/seed/"+seed+"/"+w+"/"+h+".jpg";
 }
 
 export async function getPage(
