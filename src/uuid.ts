bd4069e9d5340b3fc6e662412ca5497d496e9eeb
pfg <pfg@pfg.pw>
2022-02-09 16:04:32 -0500

    uuids with yi syllables because why not

--- src/uuid.ts
+++ src/uuid.ts
@@ -1,12 +1,23 @@
 export type UUID = string & {__is_uuid: true};
 
-/// generates a 22-24 character unique string
+/// generates a 16-character unique string (48 bytes utf-8)
 export function uuid(): UUID {
-  return btoa([
+  return [
     ...crypto.getRandomValues(new Uint8Array(16))
-  ].map(byte =>
-    String.fromCodePoint(byte)
-  ).join("")).replaceAll("=", "") as UUID;
+  ].map(byte => {
+    // // btoa chars
+    // return String.fromCodePoint(byte);
+    // (make sure to call btoa on the joined output)
+
+    // // latin characters with accents. may have normalization issues. 32 bytes utf-8.
+    // return String.fromCodePoint(byte + 256);
+
+    // // braille patterns 0..255. 48 bytes utf-8.
+    // return String.fromCodePoint(byte + 10240);
+
+    // yi syllables 0..255. 48 bytes utf-8.
+    return String.fromCodePoint(byte + 40960);
+  }).join("") as UUID;
 }
 
 // woah I need to look at uuidv5
