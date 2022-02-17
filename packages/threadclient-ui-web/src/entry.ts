3941ffd7c2d9698f12aa4198949f98bccb5ecca2
pfg <pfg@pfg.pw>
2022-02-17 17:21:11 -0500

    wow, that was fast

--- packages/threadclient-ui-web/src/entry.ts
+++ packages/threadclient-ui-web/src/entry.ts
@@ -4,6 +4,4 @@ import "./main2.scss";
 import "./_stdlib";
 import {main} from "./router";
 
-(window as unknown as {global: typeof window}).global = window; // https://github.com/awto/effectfuljs/issues/29
-
 main();
