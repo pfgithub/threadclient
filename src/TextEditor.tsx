ea337f053230b6cb0e99fd24825f5077a98db42f
pfg <pfg@pfg.pw>
2022-02-18 17:46:11 -0500

    note

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -16,11 +16,30 @@ export type RootState = {
     setData: SetStoreFunction<{data: unknown}>,
 };
 
+// // a little composable leaf node
+// // for now it's just an <input /> but eventually we should probably do
+// // make it a real TextNode. the point is it handles selection and stuff
+//
+// export function Leaf(): JSX.Element {
+//
+// }
+
 // I think I want to make this out of little tiny editable leaf nodes
 // that we put event handlers on to say what happens when you move out of them
 
 // so like the leaf nodes handle their own movement or something idk
 // and we broadcast up to ask how to move around
+
+// ok here's what we want
+// model:
+// root: <div class="min-h-[130px] p-2 bg-gray-700 rounded-md space-y-4">
+//   paragraph[]: <p
+//     text[] {bold: boolean}
+//
+// and then a bunch of fancy interactions:
+// - merge two texts if they have the same attributes
+// - pressing enter in a text should insert a newline. if there is already
+//   a newline, it should make a new paragraph.
 
 export function TextEditor(props: {
     state: RootState,
