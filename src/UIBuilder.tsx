5d720a67ed4b434b63966e16ea14b4ed49f519f9
pfg <pfg@pfg.pw>
2022-02-18 15:41:47 -0500

    idea

---
+++ src/UIBuilder.tsx
@@ -0,0 +1,8 @@
+// drag and drop ui builder
+// the goal is to make it support like any component you want
+// we'll see
+// and it should be designed for either imgui or solid js reactive output
+// so like for lists you can make a list item spec and it just makes a component
+// and you can add holes to the list that are filled in with code eg
+
+// oh yeah and no constraints. constraints are horrible. use flex/grid instead.
