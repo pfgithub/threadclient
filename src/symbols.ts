55cc3fb9507a15531da0f65245183f2bd8804321
pfg <pfg@pfg.pw>
2022-02-24 13:49:55 -0500

    note

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,3 +1,7 @@
+// this is just for hmr
+// ideally everywhere we're using these should be replaced
+// with a signal instead.
+
 export const object_active_field = Symbol("object_active_field");
 export const text_editor_selection = Symbol("text_editor_selection");
 export const json_viewer_view_mode = Symbol("json_viewer_view_mode");
