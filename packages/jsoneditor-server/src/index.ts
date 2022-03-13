352685a644678ee3f87e4235da1ad6e7b7106222
pfg <pfg@pfg.pw>
2022-03-13 15:53:46 -0400

    a plan

---
+++ packages/jsoneditor-server/src/index.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import initExpress from "express";
+import init_http from "http";
+import { MongoClient } from "mongodb";
+import { Server as SocketIOServer } from "socket.io";
+
+const mongo = new MongoClient("ABCDEFG");
+
+const express = initExpress();
+
+const http = init_http.createServer(express);
+
+const io = new SocketIOServer(http);
+
+express.use(cors());
+
+(async () => {
+    await mongo.connect();
+    const collection = mongo.db("jsoneditor").collection("data");
+    console.log("Listening", http.address());
+})().catch(e => {
+    console.error(e);
+});
