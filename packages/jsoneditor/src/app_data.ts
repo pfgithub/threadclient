dcb17bfb33f5628067f60f66c2d670bbf137763e
pfg <pfg@pfg.pw>
2022-03-14 14:30:51 -0400

    action based app_data. it doesn't yet use snapshots so it scales horribly.

--- packages/jsoneditor/src/app_data.ts
+++ packages/jsoneditor/src/app_data.ts
@@ -4,6 +4,7 @@
 //     and then one big object that is the actual value
 
 import { batch, createMemo, createSignal, Signal, untrack } from "solid-js";
+import { assertNever } from "tmeta-util";
 import { asObject, isObject, unreachable } from "./guards";
 import { Include } from "./util";
 
@@ -78,6 +79,9 @@ export function anSetReconcile<T>(node: AnNodeData<T>, nv: (pv: unknown) => T): 
 export function anSetReconcileIncomplete<T>(node: AnNodeData<T>, nv: (pv: unknown) => Partial<T>): void {
     setReconcile(node[symbol_root], node[symbol_path], nv);
 }
+// export function anObserve(node: node): the actual value of the node. all children are watched
+// eg we can get a node signal [...node[symbol_path], {v: "@all"}] and update all of those whenever
+// any node is changed.
 
 (() => {
     type Person = {
@@ -101,21 +105,99 @@ export function anSetReconcileIncomplete<T>(node: AnNodeData<T>, nv: (pv: unknow
 
 type Primitive = string | bigint | boolean | null | undefined;
 
+type JSON = unknown;
+type ActionPath = string[];
+type ActionValue = {
+    kind: "reorder_keys",
+    path: ActionPath,
+    old_keys: string[],
+    new_keys: string[],
+    //^ so if one person adds a key while another person reorders, the added item
+    //  does not get deleted.
+} | {
+    kind: "set_value",
+    path: ActionPath,
+    new_value: JSON,
+};
+type Action = {
+    // id: string,
+    value: ActionValue,
+}
+
 export type Root = {
     // note: see if we can use a weakmap for this
     // we'll have to use the same path array for every watched nodes access
     // eeh whatever. we'll just leak memory
     watched_nodes: Map<string, Signal<undefined>>,
-    data: unknown, // [!] acyclic, no references, readonly. must be updated using reconcile()
     all_contents: Set<object>, // [!] debug builds only, used to automatically detect and error for references.
 
-    // if I want I can store the actual data inside of nodes but idk
+    actions: Action[],
+    // snapshots: Map<ActionHash, JSON>, // TODO how do we do snapshots?
+    snapshot: JSON,
 };
+function applyActions(actions: Action[]): JSON {
+    // TODO find an existing snapshot to base this off rather than recreating from
+    // scratch
+
+    let snapshot: JSON = undefined;
+    for(const action of actions) {
+        snapshot = applyActionToSnapshot(action, snapshot)
+    }
+    return snapshot;
+}
+function setNodeAtPath(path: ActionPath, snapshot: JSON, upd: (prev: JSON) => JSON): JSON {
+    console.log("setting node at path", path);
+
+    const res_root: {parent: {[key: string]: unknown}, key: string} = {
+        parent: {r: {...asObject(snapshot) ?? {}}},
+        key: "r",
+    };
+    let res = res_root;
+    for(const key of path) {
+        console.log(res);
+        const newnode = {...asObject(res.parent[res.key]) ?? {}};
+        res.parent[res.key] = newnode;
+        res = {
+            parent: newnode,
+            key,
+        };
+    }
+    res.parent[res.key] = upd(res.parent[res.key]);
+    console.log("succeed set node", res_root);
+    return res_root.parent[res_root.key];
+}
+function applyActionToSnapshot(action: Action, snapshot: JSON): JSON {
+    const av = action.value;
+    if(av.kind === "set_value") {
+        return setNodeAtPath(av.path, snapshot, prev => {
+            return av.new_value;
+        });
+    }else if(av.kind === "reorder_keys") {
+        return setNodeAtPath(av.path, snapshot, prev => {
+            if(!isObject(prev)) return;
+
+            // this logic can be improved to be better
+            const current_keys = Object.keys(prev);
+            const expected_current_keys = av.old_keys;
+
+            const missing_current_keys = new Set(expected_current_keys);
+            for(const key of current_keys) missing_current_keys.delete(key);
+            const extra_current_keys = new Set(current_keys);
+            for(const key of expected_current_keys) extra_current_keys.delete(key);
+
+            return Object.fromEntries([...av.new_keys, ...extra_current_keys].map((key) => {
+                return [key, prev[key]];
+            }));
+        });
+    }else assertNever(av);
+}
 export function createAppData<T>(): AnNode<T> {
     const root: Root = {
         watched_nodes: new Map(),
-        data: undefined,
         all_contents: new Set(),
+
+        actions: [],
+        snapshot: undefined,
     };
     return anConstructor(root, []);
 }
@@ -151,7 +233,7 @@ function readValue(root: Root, path: string[]): unknown {
     // track reads
     getNodeSignal(root, path).view();
 
-    let ntry = root.data;
+    let ntry = root.snapshot;
     for(const itm of path) {
         if(isObject(ntry)) {
             ntry = ntry[itm];
@@ -162,57 +244,60 @@ function readValue(root: Root, path: string[]): unknown {
     }
     return ntry;
 }
+// change how setreconcile works
+// 1: find actions
+// 2: applyActions
+// 3: emitDiffSignals
+
+function findActions(path: string[], pv: JSON, nv: JSON): Action[] {
+    if(pv === nv) return [];
+
+    if(!isObject(pv) || !isObject(nv)) {
+        return [{value: {
+            kind: "set_value",
+            path: path,
+            new_value: nv,
+        }}];
+    }
+
+    const res_actions: Action[] = [];
+
+    const prev_keys = Object.keys(pv);
+    const next_keys = Object.keys(nv);
+
+    // change key order & delete old keys
+    if(JSON.stringify(prev_keys) !== JSON.stringify(next_keys)) res_actions.push({value: {
+        kind: "reorder_keys",
+        path: path,
+        old_keys: prev_keys,
+        new_keys: next_keys,
+    }});
+
+    // create all new keys
+    for(const key of next_keys) {
+        res_actions.push(...findActions([...path, key], pv[key], nv[key]));
+    }
+
+    return res_actions;
+}
 
 function setReconcile<T>(root: Root, path: string[], nvCb: (pv: unknown) => T): void {
     untrack(() => batch(() => {
         const pv = readValue(root, path);
         const nv = nvCb(pv);
 
-        let ntry: {
-            parent: {[key: string]: unknown},
-            key: string,
-        } = {
-            parent: root,
-            key: "data",
-        };
-        for(const [i, path_node] of path.entries()) {
-            let v = ntry.parent[ntry.key];
-            if(Object.hasOwnProperty.call(ntry.parent, ntry.key) && isObject(v)) {
-                // we're good!
-            } else {
-                // turn it into an object
-                v = {};
-                if(!Object.hasOwnProperty.call(ntry.parent, ntry.key) && i - 1 >= 0) {
-                    const parent_path = path.slice(0, i - 1);
-                    getNodeSignal(root, [...parent_path, {v: "keys"}]).emit();
-                }
-                ntry.parent[ntry.key] = v;
+        const new_actions = findActions(path, pv, nv);
+        root.actions.push(...new_actions);
+    
+        const ps = root.snapshot;
+        const ns = applyActions(root.actions);
+        root.snapshot = ns;
 
-                const obj_path = path.slice(0, i);
-                console.log("had to create object at", JSON.stringify(obj_path));
-                getNodeSignal(root, obj_path).emit();
-            }
-            ntry = {
-                parent: v as {[key: string]: unknown},
-                key: path_node,
-            };
-        }
-
-        if(!Object.hasOwnProperty.call(ntry.parent, ntry.key)) {
-            // [!] duplicated code
-            const pathlen = path.length;
-            if(pathlen - 1 >= 0) {
-                const parent_path = path.slice(0, pathlen - 1);
-                getNodeSignal(root, [...parent_path, {v: "keys"}]).emit();
-            }
-        }
-
-        // [!] we might end up with stale references. if that's not acceptable, we'll have
-        //     to reconcile properly. this should be okay though I think.
-        ntry.parent[ntry.key] = nv;
+        console.log("Applying actions:", new_actions);
+        console.log("Got result", ns);
 
         // emit its signals
-        emitDiffSignals(root, path, pv, nv);
+        emitDiffSignals(root, [], ps, ns);
     }));
 }
 function emitDiffSignals<T>(root: Root, path: string[], old_value: unknown, new_value: T): void {
@@ -251,7 +336,6 @@ function emitDiffSignals<T>(root: Root, path: string[], old_value: unknown, new_
     if(isObject(new_value)) {
         const pv = asObject(old_value) ?? {};
         for(const key of new_keys) {
-            console.log("emit4key", key, pv[key], new_value[key]);
             emitDiffSignals(root, [...path, key], pv[key], new_value[key]);
         }
     }
