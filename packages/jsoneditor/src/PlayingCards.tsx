54bc5175cdde478043664ccbfd726ccb0651302a
pfg <pfg@pfg.pw>
2022-03-14 17:13:12 -0400

    starting on this playingcards thing. it generates *a lot* of actions

--- packages/jsoneditor/src/PlayingCards.tsx
+++ packages/jsoneditor/src/PlayingCards.tsx
@@ -1,6 +1,6 @@
 import { createSignal, JSX, onCleanup, onMount } from "solid-js";
 import { insert } from "solid-js/web";
-import { AnNode, anNumber, anSetReconcile } from "./app_data";
+import { anBool, AnNode, anNumber, anSetReconcile } from "./app_data";
 import { Buttons, Button } from "./components";
 import { AnFor } from "./Schemaless";
 import { uuid } from "./uuid";
@@ -18,18 +18,35 @@ type PlayingCards = {
 
 type Test = Parameters<(typeof document)["addEventListener"]>;
 
+function toZIndex(number: number): number {
+    // good enough for now but everything will get stuck on top when the time
+    // rolls over the 32 bit integer mark
+    //
+    // so basically we have to make some messy signal thing to put all times onto
+    // one scale and sort them
+
+    // this should:
+    // - add the current time to 
+    // - onCleanup() remove it
+    // - return a signal of the index of the number
+
+    return number % 2**32 - 2**31;
+}
+
 function Token(props: {token: AnNode<Token>, width: number, container: HTMLElement}): JSX.Element {
     let cleanup_fn: (() => void) | undefined;
     onCleanup(() => cleanup_fn?.());
 
     return <div
-        class="bg-gray-500"
+        class="bg-gray-500 border-[2px] border-gray-900"
         style={{
-            position: "absolute",
-            top: (anNumber(props.token.pos.y) ?? 0) * 100 + "%",
-            left: (anNumber(props.token.pos.x) ?? 0) * 100 + "%",
-            transform: "translate(-50%, -50%)",
-            padding: (0.005) * 100 + "%",
+            'position': "absolute",
+            'top': (anNumber(props.token.pos.y) ?? 0) * 100 + "%",
+            'left': (anNumber(props.token.pos.x) ?? 0) * 100 + "%",
+            'transform-origin': "top left",
+            'transform': anBool(props.token.dragging) ? "scale(1.2) translate(-50%, -50%)" : "translate(-50%, -50%)",
+            'padding': (0.005) * 100 + "%",
+            'z-index': toZIndex(anNumber(props.token.upd) ?? 0),
         }}
         onPointerDown={initial_ev => {
             if(!initial_ev.isPrimary) return;
@@ -39,6 +56,7 @@ function Token(props: {token: AnNode<Token>, width: number, container: HTMLEleme
             initial_ev.stopPropagation();
 
             anSetReconcile(props.token.dragging, () => true);
+            anSetReconcile(props.token.upd, () => Date.now()); // {'@operation': "date-now"}
 
             const pxToPt = (px: [number, number]): [number, number] => {
                 const container_rect = props.container.getBoundingClientRect();
--- packages/jsoneditor/src/ServerExample.tsx
+++ packages/jsoneditor/src/ServerExample.tsx
@@ -2,27 +2,17 @@ import { createSignal, JSX, onCleanup } from "solid-js";
 import { io } from "socket.io-client";
 import { Show } from "tmeta-util-solid";
 
-// ok we get to pick:
-// should we recieve:
-// - all events related to a document
-// or:
-// - only events related to anything we subscribe to
-
-// choices:
-// - all events means that the app won't work as well with large documents but
-//   significantly simplifies stuff
-// - only events we subscribe to is nice but i think it makes the reconcile() function
-//   in app_data harder
-//   - oh this would be really slow wouldn't it. we'd ask for keys and then based on
-//     the result ask for more data and then it would end up not being very good
-//
-// ok so thinking about all events:
-// - change everything into an action. so when you reconcile, it appends a batch of
-//   actions to the action list
-// - when we recieve an event from the server, we apply the action
-//
-// - ok so I'm going to reorganize app_data around actions and then we can start
-//   working with the server probably
+// [!] batch actions by ~200ms before submitting to the server
+//   - we can make a fn to batchActions(actions) that will merge any duplicate sets
+//   - this is for playingcards and anything else that has fast input from users
+// [!] we need more types of actions or we need some way to define special values within
+//     reconcile
+//   - eg say we have a counter with + and -. when two people click + at once, both adds
+//     should be counted. this involves either making a special incrementNumber action or
+//     using a special value like `{"@operation": "++", "default": 0}` that when the
+//     server recieves it it uses to create an atomic number increment in the database
+//   - eg like `{@operation: "date-now"}` and it will insert using the client's date
+//     at first but then update to the server's date once the insert happens on the server
 
 export default function ServerExample(): JSX.Element {
     const connection = io("http://localhost:3564/document-example")
