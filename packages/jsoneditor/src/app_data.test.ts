ac3d4b6aaf8bb82da45bb8a1ff6b123c3b946cdb
pfg <pfg@pfg.pw>
2022-03-18 15:23:08 -0400

    hmm. I think i'm just going to write some tests for app_data and not try to figure out future stuff

--- packages/jsoneditor/src/app_data.test.ts
+++ packages/jsoneditor/src/app_data.test.ts
@@ -1,5 +1,5 @@
 import tap from "tap";
-import { anRoot, anSetReconcile, createAppData } from "./app_data";
+import { anCreateUndoGroup, anRoot, anSetReconcile, anSetReconcileIncomplete, anUndo, createAppData } from "./app_data";
 import "./test_setup";
 
 void tap.test("program", async () => {
@@ -14,4 +14,62 @@ void tap.test("program", async () => {
     tap.same(anRoot(data).snapshot, {
         name: "Julia",
     });
+});
+
+// hmm i'm not sure about this
+void tap.test("deleting objects should delete any changes made", async () => {
+    type Person = {
+        name: string,
+        description: string,
+    };
+    const data = createAppData<{[key: string]: Person}>();
+    const root = anRoot(data);
+
+    tap.same(anRoot(data).snapshot, undefined);
+
+    // Client 1 creates a user
+
+    const undo_group = anCreateUndoGroup();
+    anSetReconcileIncomplete(data["julia"]!, () => ({}), {undo_group});
+
+    tap.same(anRoot(data).snapshot, {
+        julia: {},
+    });
+
+    // Client 2 sets the user's name
+
+    anSetReconcileIncomplete(data["julia"]!.name, () => "Julia");
+
+    tap.same(anRoot(data).snapshot, {
+        julia: {
+            name: "Julia"
+        },
+    });
+
+    // Client 1 undoes the creation
+
+    anUndo(root, undo_group);
+
+    // tap.same(anRoot(data).snapshot, undefined); ← not sure if this should be correct
+    // behaviour or not. so i'm not implementing it yet.
+
+    // Client 3 has only received the first event and just now sets the user's name
+
+    anSetReconcileIncomplete(data["julia"]!.name, () => "Julia");
+
+    tap.same(anRoot(data).snapshot, {
+        julia: {
+            name: "Julia"
+        },
+    });
+
+    // ^ is this the behaviour we want? seems strange
+
+    // feels like we want:
+    // - in arrays, you have to create an item before you can add things
+    // - in objects, you can set to any key we want
+    // is that true?
+    //
+    // that's not easy to handle given that right now, arrays and objects are the same
+    // thing
 });
