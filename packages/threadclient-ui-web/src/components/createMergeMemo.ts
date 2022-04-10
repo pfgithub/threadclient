a81abac8218898522eed8e8206e9f8165929a3be
pfg <pfg@pfg.pw>
2022-04-10 14:50:25 -0400

    work around a typescript bug (# 48636)

--- packages/threadclient-ui-web/src/components/createMergeMemo.ts
+++ packages/threadclient-ui-web/src/components/createMergeMemo.ts
@@ -1,18 +1,18 @@
 import { createReaction } from "solid-js";
-import { createStore, DeepReadonly, reconcile } from "solid-js/store";
+import { createStore, reconcile } from "solid-js/store";
 
 export function createMergeMemo<T>(getValue: () => T, opts: {key: string | null, merge: boolean}): {data: T} {
     const [value, setValue] = createStore<{data: T | null}>({data: null});
 
     const track = createReaction(() => {
-        track(() => setValue("data", reconcile(getValue() as DeepReadonly<T>, {
+        track(() => setValue("data", reconcile(getValue() as any, {
             merge: opts.merge,
             key: opts.key,
         })));
     });
     
     track(() => {
-        setValue("data", reconcile(getValue() as DeepReadonly<T>));
+        setValue("data", reconcile(getValue() as any));
     });
 
     // using createReaction/track in order to make sure the effect is guarenteed
