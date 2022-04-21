883d0e0cab394e660308c2288db3d09d3307e541
pfg <pfg@pfg.pw>
2022-04-21 16:06:04 -0400

    improve reply style

--- packages/threadclient-ui-web/src/components/reply.tsx
+++ packages/threadclient-ui-web/src/components/reply.tsx
@@ -2,10 +2,18 @@ import type * as Generic from "api-types-generic";
 import { rt } from "api-types-generic";
 import { createSignal, JSX } from "solid-js";
 import { Show } from "tmeta-util-solid";
-import { getClientCached, link_styles_v } from "../app";
+import { getClientCached } from "../app";
 import { getSettings, localStorageSignal } from "../util/utils_solid";
 import { createMergeMemo } from "./createMergeMemo";
 import { ClientContent, TopLevelWrapper } from "./page2";
+
+// ok the dream for replies is to have a comment appear where you edit the actual body and it
+// previews below or on the right or something
+//
+// shouldn't be *too* difficult
+//
+// same for edit but in the comment you're editing
+// so reply should basically make a new comment and mark it as being edited
 
 export default function ReplyEditor(props: {
     action: Generic.ReplyAction,
@@ -37,74 +45,86 @@ export default function ReplyEditor(props: {
         return client!.previewReply!(content(), props.action.reply_info);
     }, {key: null, merge: true});
 
+    const sendReply = () => {
+        setSending(true);
+
+        const client = getClientCached(props.action.client_id);
+        client!.sendReply!(content(), props.action.reply_info, props.action.mode).then((r) => {
+            console.log("Got response", r);
+            // TODO:
+            // so, this is a terrible idea,
+            // but what if onAddReply sent the .boundingClientRect of the TopLevelWrapper preview
+            // so it could like animate the newly added reply into position
+            // terrible idea, but like…
+            // it could be fun
+            // might not look good
+            setBaseContent(null); // trust that the response is succesful
+            props.onAddReply({
+                kind: "post",
+                internal_data: r.raw_value,
+                content: r.kind === "thread" ? {
+                    kind: "legacy",
+                    thread: r,
+                    client_id: props.action.client_id,
+                } : {
+                    kind: "post",
+                    title: {text: "Error"},
+                    body: {kind: "richtext", content: [
+                        rt.p(rt.error("Unsupported 'load more'", r)),
+                    ]},
+                    collapsible: false,
+                },
+                display_style: "centered",
+
+                parent: null,
+                replies: null,
+                url: null,
+                client_id: props.action.client_id,
+            });
+        }).catch((error) => {
+            setSending(false);
+            const err = error as Error;
+            console.log("Got error", err);
+            setSendError(err.toString() + "\n" + err.stack);
+        });
+    };
+    const cancelReply = () => {
+        props.onCancel();
+    };
+
     return <div>
         <textarea
+            class="
+                block w-full rounded-t-lg resize-y
+                bg-transparent border-2 border-b-0 border-zinc-700 p-2
+            "
+            rows={4}
             disabled={isSending()}
-            class="border my-3 w-full resize-y"
             value={baseContent() ?? ""}
             onInput={(e) => {
                 setBaseContent(e.currentTarget.value);
             }}
         />
-        <Show if={!!settings.signature().trim()}>
-            <label class="my-3">
-                <input type="checkbox" checked={showSignature()} onInput={nv => {
-                    setShowSignature(nv.currentTarget.checked);
-                }} />
-                {" "}Show my signature on this post
-            </label>
-        </Show>
-        <div class="flex space-x-1 my-3">
-            <button
-                disabled={empty() || isSending()}
-                class={link_styles_v[empty() ? "pill-empty" : "pill-filled"]}
-                onClick={(e) => {
-                    setSending(true);
-
-                    const client = getClientCached(props.action.client_id);
-                    client!.sendReply!(content(), props.action.reply_info, props.action.mode).then((r) => {
-                        console.log("Got response", r);
-                        // TODO:
-                        // so, this is a terrible idea,
-                        // but what if onAddReply sent the .boundingClientRect of the TopLevelWrapper preview
-                        // so it could like animate the newly added reply into position
-                        // terrible idea, but like…
-                        // it could be fun
-                        // might not look good
-                        setBaseContent(null); // trust that the response is succesful
-                        props.onAddReply({
-                            kind: "post",
-                            internal_data: r.raw_value,
-                            content: r.kind === "thread" ? {
-                                kind: "legacy",
-                                thread: r,
-                                client_id: props.action.client_id,
-                            } : {
-                                kind: "post",
-                                title: {text: "Error"},
-                                body: {kind: "richtext", content: [
-                                    rt.p(rt.error("Unsupported 'load more'", r)),
-                                ]},
-                                collapsible: false,
-                            },
-                            display_style: "centered",
-
-                            parent: null,
-                            replies: null,
-                            url: null,
-                            client_id: props.action.client_id,
-                        });
-                    }).catch((error) => {
-                        setSending(false);
-                        const err = error as Error;
-                        console.log("Got error", err);
-                        setSendError(err.toString() + "\n" + err.stack);
-                    });
-                }}
-            >{isSending() ? "…" : props.action.text}</button>
-            <button disabled={isSending()} class={link_styles_v["pill-empty"]} onClick={(e) => {
-                props.onCancel();
-            }}>Cancel<div /></button>
+        <div class="bg-zinc-700 rounded-b-lg p-2 flex flex-col gap-2">
+            <Show if={!!settings.signature().trim()}>
+                <label>
+                    <input type="checkbox" checked={showSignature()} onInput={nv => {
+                        setShowSignature(nv.currentTarget.checked);
+                    }} />
+                    {" "}Show my signature on this post
+                </label>
+            </Show>
+            <div class="flex flex-wrap gap-2">
+                <button
+                    class={(empty() ? "bg-zinc-100" : "bg-blue-400") + " rounded-md text-zinc-900 py-1 px-3"}
+                    disabled={empty() || isSending()}
+                    onClick={sendReply}
+                >Reply</button>
+                <button
+                    onClick={cancelReply}
+                    disabled={isSending()}
+                >Cancel</button>
+            </div>
         </div>
         <Show when={sendError()}>{errv => <>
             <pre class="error"><code>There was an error! {errv}</code></pre>
