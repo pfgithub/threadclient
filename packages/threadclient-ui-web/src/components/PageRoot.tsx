91b87c809d2e29648a3faac5f515bd33d94e671f
pfg <pfg@pfg.pw>
2022-04-21 15:20:35 -0400

    tried out making a sidebar for the sidebar button. it doesn't work at all. reverting next commit.

--- packages/threadclient-ui-web/src/components/PageRoot.tsx
+++ packages/threadclient-ui-web/src/components/PageRoot.tsx
@@ -1,18 +1,21 @@
 import type * as Generic from "api-types-generic";
 import { readLink } from "api-types-generic";
+import { Dialog, DialogOverlay, DialogTitle, Transition, TransitionChild } from "solid-headless";
 import {
+    createEffect,
     createMemo,
     createSignal,
-    For, JSX, untrack
+    For, JSX, on, onCleanup, untrack
 } from "solid-js";
+import { Portal } from "solid-js/web";
 import { Show } from "tmeta-util-solid";
 import { getWholePageRootContext, size_lt } from "../util/utils_solid";
 import { createMergeMemo } from "./createMergeMemo";
 import { CollapseData, flatten } from "./flatten";
+import { InternalIconRaw } from "./Icon";
 import LandingPage from "./LandingPage";
 import PageFlatItem from "./PageFlatItem";
 import { array_key } from "./symbols";
-import ToggleButton from "./ToggleButton";
 
 type SpecialCallback = () => PageRes;
 const full_page_special_callbacks: Record<string, SpecialCallback> = {
@@ -79,12 +82,14 @@ function ClientPageMain(props: ClientPageProps): PageRes {
 
     // 320px sidebar
 
-    const tabbed = createMemo(() => size_lt.lg() && view.data.sidebar != null);
+    const hasSidebar = createMemo(() => size_lt.lg() && view.data.sidebar != null);
 
     // alternatively, we can have the sidebar be guarenteed to have a url and then the "sidebar" button would
     // repivot to the parent sidebar node. this could be nice because it would allow navigation with the
     // browser back button and stuff.
-    const [tab, setTab] = createSignal<"content" | "sidebar">("content");
+    const [showSidebar, setShowSidebar] = createSignal(false);
+
+    createEffect(on([() => props.pivot], () => setShowSidebar(false), {defer: true}));
 
     return {get title() {
         return view.data.title;
@@ -94,31 +99,27 @@ function ClientPageMain(props: ClientPageProps): PageRes {
                 TODO show header
             </div>
         </Show>
-        <Show if={tabbed()}>
-            <div class="flex flex-col items-center justify-center">
-                <ToggleButton
-                    value={tab()}
-                    setValue={v => v ? setTab(v) : void 0}
-                    choices={[
-                        ["content" as const, <>Content</>],
-                        ["sidebar" as const, <>Sidebar</>],
-                    ]}
-                />
+        <Show if={hasSidebar()}>
+            <div class="<sm:px-4">
+                <button
+                    class="bg-gray-200 rounded-md p-2 px-3"
+                    onClick={() => setShowSidebar(v => !v)}
+                >
+                    Sidebar
+                </button>
             </div>
         </Show>
         <div class="flex flex-row gap-8 justify-center">
-            <Show if={tabbed() ? tab() === "content" : true}>
-                <div class="sm:max-w-4xl w-full">
-                    <For each={view.data.body}>{item => (
-                        <PageFlatItem
-                            item={item}
-                            collapse_data={collapse_data}
-                        />
-                    )}</For>
-                </div>
-            </Show>
-            <Show if={view.data.sidebar != null && (tabbed() ? tab() === "sidebar" : true)}>
-                <div class="sm:max-w-320px w-full">
+            <div class="max-w-4xl w-full">
+                <For each={view.data.body}>{item => (
+                    <PageFlatItem
+                        item={item}
+                        collapse_data={collapse_data}
+                    />
+                )}</For>
+            </div>
+            <Show if={view.data.sidebar != null && !hasSidebar()}>
+                <div class="max-w-320px w-full">
                     <For each={view.data.sidebar}>{item => (
                         <PageFlatItem
                             item={item}
@@ -126,6 +127,55 @@ function ClientPageMain(props: ClientPageProps): PageRes {
                         />
                     )}</For>
                 </div>
+            </Show>
+            <Show if={hasSidebar() && showSidebar()}>
+                {untrack(() => {
+                    // ios doesn't support "overscroll-contain"
+                    document.documentElement.style.overflow = "hidden";
+                    onCleanup(() => document.documentElement.style.overflow = "");
+                    return undefined;
+                })}
+                <Portal mount={el("div").adto(document.body)}>
+                    <Dialog isOpen onClose={() => setShowSidebar(false)}>
+                        <div class="
+                            fixed inset-0 flex h-full flex-row flex-wrap
+                            overflow-y-scroll overscroll-contain justify-start
+                            bg-slate-500 dark:bg-zinc-700 bg-opacity-75 dark:bg-opacity-75
+                        " style={{'-webkit-overflow-scrolling': "touch"}}>
+                            <DialogOverlay class="flex-1" />
+                            <button
+                                type="button"
+                                class="
+                                    flex
+                                    text-slate-900 dark:text-zinc-100
+                                    rounded-md  hover:text-black
+                                    focus:outline-none focus:ring-2
+                                "
+                                onClick={() => setShowSidebar(false)}
+                            ><div>
+                                <div class="sticky top-0 left-0 right-0 p-4">
+                                    <InternalIconRaw class="fa-solid fa-x" label="Close panel" />
+                                </div>
+                            </div></button>
+                            <div class="
+                                bg-slate-300 dark:bg-zinc-900 shadow-lg py-6 max-w-320px
+                                w-max
+                            ">
+                                <div class="px-4 sm:px-6">
+                                    <DialogTitle class="text-lg font-medium text-gray-900">Sidebar</DialogTitle>
+                                </div>
+                                <div class="relative mt-6 flex-1 sm:px-6">
+                                    <For each={view.data.sidebar}>{item => (
+                                        <PageFlatItem
+                                            item={item}
+                                            collapse_data={collapse_data}
+                                        />
+                                    )}</For>
+                                </div>
+                            </div>
+                        </div>
+                    </Dialog>
+                </Portal>
             </Show>
         </div>
     </div>};
