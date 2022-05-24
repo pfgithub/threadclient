8babb8c9eda7c85b16088f139436886939a1ee6e
pfg <pfg@pfg.pw>
2022-05-24 15:51:07 -0400

    I could make my homepage like this

--- packages/threadclient-ui-web/src/experiments/term_app/TermApp.tsx
+++ packages/threadclient-ui-web/src/experiments/term_app/TermApp.tsx
@@ -33,6 +33,52 @@ async function adventureGame(t: Term): Promise<void> {
     }
 }
 
+async function projectsApp(t: Term): Promise<void> {
+    while(true) {
+        const [printcmd, setPrintcmd] = createSignal<string>("");
+        t.print(<Line class="bg-zinc-900">/projects$ {printcmd()}</Line>);
+        const v = (await t.read({
+            onProgress: q => void setPrintcmd(q),
+        })).split(" ");
+        setPrintcmd(v.join(" ")+"");
+
+        if(v[0] === "ls") {
+            if(v[1] == null) {
+                t.print(<Line>current/ past/</Line>);
+            }else if(v[1] === "current") {
+                t.print(<div class="m-2"><div class="bg-slate-300 text-slate-900">
+                    <div class="max-w-screen-lg mx-auto">
+                        <div class="p-4">
+                            <h2 class="font-black text-3xl my-3">Current Projects</h2>
+                            <p class="mb-3">Large projects I'm working on right now</p>
+                            <div class="my-2 flex flex-col sm:flex-row hover:shadow-md bg-slate-100 hover:bg-hex-fff">
+                                <div class="sm:w-40 sm:h-auto flex-none overflow-hidden" aria-hidden="true">
+                                    <a rel="noopener" target="" tabindex="-1" href="/projects/interpunct_bot">
+                                        <img src="https://pfg.pw/icons/interpunct_img.png" alt="" class="w-full h-full object-cover " width="960" height="600" />
+                                    </a>
+                                </div>
+                                <div class="p-4 flex flex-col z-10 relative">
+                                    <h3>
+                                        <a rel="noopener" target="" class="font-black hover:underline" href="https://pfg.pw/projects/interpunct_bot">inter·punct bot</a>
+                                    </h3>
+                                    <div class="mb-2 mt-1">A bot for the chat service <a href="https://discord.com" class="hover:underline text-blue-900" target="_blank" rel="noopener">Discord</a> that adds many features including games such as Checkers and Paper Soccer and moderator functions such as a ticketing system and a way for mods to give people roles faster using emojis.</div>
+                                    <div class="font-light text-sm">
+                                        <a href="https://www.typescriptlang.org/" class="hover:underline" target="_blank" rel="noopener">Typescript</a> • <a href="https://nodejs.org/en/" class="hover:underline" target="_blank" rel="noopener">NodeJS</a> • <a href="https://discord.js.org/" class="hover:underline" target="_blank" rel="noopener">discord.js</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div></div>);
+            }else{
+                t.print(<Line>file not found</Line>);
+            }
+        }else{
+            t.print(<Line>I'm not sure what you mean. `help` for help.</Line>);
+        }
+    }
+}
+
 async function app(t: Term): Promise<void> {
     while(true) {
         const [printcmd, setPrintcmd] = createSignal<string>("");
@@ -48,6 +94,13 @@ async function app(t: Term): Promise<void> {
             }catch(er) {
                 const e = er as Error;
                 t.print(<Line>Adventure game errored: {e.toString() + "\n" + e.stack}</Line>);
+            }
+        }else if(text === "projects") {
+            try {
+                await projectsApp(t);
+            }catch(er) {
+                const e = er as Error;
+                t.print(<Line>Projects app errored: {e.toString() + "\n" + e.stack}</Line>);
             }
         }else if(text === "exit") {
             t.print(<Line>Goodbye.</Line>);
