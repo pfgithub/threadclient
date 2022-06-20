4c3be8d3e66f0896d8a93dcc7d61907645e7f989
pfg <pfg@pfg.pw>
2022-06-20 12:06:58 -0400

    <Menu> li.display:contents

--- packages/threadclient-ui-web/src/components/PageFlatItem.tsx
+++ packages/threadclient-ui-web/src/components/PageFlatItem.tsx
@@ -112,16 +112,17 @@ function PageFlatItemNoError(props: {item: FlatItem, collapse_data: CollapseData
             {" "}{fsb.name}
         </A>,
         sort_buttons: sortbtns => <ToggleColor>{color => <div class={"mt-4 mb-4 rounded-lg "+color}>
-            <div class="p-3 flex flex-row flex-wrap gap-2 dark:text-zinc-400">
-                <For each={sortbtns.sort_buttons}>{sortbtn => {
-                    // inline-block mx-1 px-1 text-base border-b-2 transition-colors border-transparent text-gray-600 dark:text-gray-400 hover:text-gray-900
-                    return <SwitchKind item={sortbtn}>{{
+            <menu class="p-2 flex flex-row flex-wrap gap-2 dark:text-zinc-400">
+                <For each={sortbtns.sort_buttons}>{sortbtn => <li style={{display: "contents"}}>
+                    {// inline-block mx-1 px-1 text-base border-b-2 transition-colors border-transparent text-gray-600 dark:text-gray-400 hover:text-gray-900
+                    }
+                    <SwitchKind item={sortbtn}>{{
                         url: s => <A href={s.url} client_id={sortbtns.client_id} class="px-1 border-b-2 border-transparent dark:hover:text-zinc-50">{sortbtn.name}</A>,
                         post: s => <>TODO {s.name}</>,
                         more: s => <A class="px-1 border-b-2 border-transparent dark:hover:text-zinc-50">{sortbtn.name} ▾</A>,
-                    }}</SwitchKind>;
-                }}</For>
-            </div>
+                    }}</SwitchKind>
+                </li>}</For>
+            </menu>
         </div>}</ToggleColor>,
         post: loader_or_post => <PageFlatPost
             collapse_data={props.collapse_data}
