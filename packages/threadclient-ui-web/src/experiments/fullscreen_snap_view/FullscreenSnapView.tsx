79d86801d28e5c71fefd376d9cc8c9c162b7b88d
pfg <pfg@pfg.pw>
2022-06-21 18:42:42 -0400

    fix the intersectionobserver element messing up the scroll

--- packages/threadclient-ui-web/src/experiments/fullscreen_snap_view/FullscreenSnapView.tsx
+++ packages/threadclient-ui-web/src/experiments/fullscreen_snap_view/FullscreenSnapView.tsx
@@ -492,8 +492,8 @@ export default function FullscreenSnapView(props: {
     onCleanup(() => visualViewport.removeEventListener("resize", rszel));
 
     return <div class={
-        "bg-hex-000 h-screen overflow-y-scroll snap-mandatory text-zinc-100 space-y-8 overflow-hidden "
-        +(zoomed() != null ? "" : "snap-y")
+        "bg-hex-000 h-screen overflow-hidden snap-mandatory text-zinc-100 space-y-8 "
+        +(zoomed() != null ? "" : "snap-y overflow-y-scroll")
     } style={{
         'touch-action': "auto",
     }}>
