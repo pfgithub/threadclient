58c29eb7bb078a936ac247c011bfd25f1ce74aa2
pfg <pfg@pfg.pw>
2022-09-24 20:22:38 -0400

    start working on a new header

--- packages/threadclient-ui-web/src/components/Header.tsx
+++ packages/threadclient-ui-web/src/components/Header.tsx
@@ -1,8 +1,18 @@
 import * as Generic from "api-types-generic";
 import { JSX } from "solid-js";
+import { SwitchKind } from "tmeta-util-solid";
 import { bioRender } from "../page1";
-import { SolidToVanillaBoundary } from "./LinkHelper";
+import { SolidToVanillaBoundary } from "../util/interop_solid";
 import { ClientPostOpts } from "./Post";
+import proxyURL from "./proxy_url";
+
+/*
+
+- banner
+- icon, title. grid overlap banner.
+- 
+
+*/
 
 export default function Header(props: {
     header: Generic.FilledIdentityCard,
@@ -16,4 +26,35 @@ export default function Header(props: {
             return resdiv;
         }} />
     </div>;
+}
+
+export function Header2(props: {
+    header: Generic.FilledIdentityCard,
+    opts: ClientPostOpts,
+}): JSX.Element {
+    // we have to un-indent
+    // do we just do negative margin?
+    // - that might overlap other objects
+    //   - negative margin but only if it's the flat top/bottom?
+    return <div>
+        <Banner banner={props.header.theme.banner} />
+    </div>;
+}
+
+export function Banner(props: {banner: Generic.Banner}): JSX.Element {
+    return <SwitchKind item={props.banner ?? {kind: "none" as const}} children={{
+        image: (bimg) => <>
+            <FullscreenViewer img_src={bimg.desktop}>
+                <img src={proxyURL(bimg.desktop)} alt="" />
+            </FullscreenViewer>
+        </>,
+        color: (bcolor) => <>
+        </>,
+        none: () => <>
+        </>,
+    }} />;
+}
+
+export function FullscreenViewer(props: {img_src: string, children: JSX.Element}): JSX.Element {
+    return <div>{props.children}</div>;
 }
