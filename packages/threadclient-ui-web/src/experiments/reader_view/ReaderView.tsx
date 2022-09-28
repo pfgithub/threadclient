4b28d8fc056935219c6c3f0e508b85e25d182bc8
pfg <pfg@pfg.pw>
2022-09-28 17:38:48 -0400

    make the reader button stop erroring all the time

--- packages/threadclient-ui-web/src/experiments/reader_view/ReaderView.tsx
+++ packages/threadclient-ui-web/src/experiments/reader_view/ReaderView.tsx
@@ -15,14 +15,14 @@ export default function ReaderView(props: {
     pivot: Generic.Link<Generic.Post>,
 }): JSX.Element {
     const hprc = getWholePageRootContext();
-    const pivotedPost = createMemo((): {v: Generic.Post, content: Generic.PostContentPost} => {
+    const pivotedPost = createMemo((): {v: Generic.Post, content: null | Generic.PostContentPost} => {
         const res = hprc.content().view(props.pivot);
         if(res == null || res.error != null) throw new Error("rve");
         const v = res.value;
         if(v.kind === "post" && v.content.kind === "post") {
             return {v, content: v.content};
         }
-        throw new Error("eunsupported");
+        return {v, content: null};
     });
     const settings = getSettings();
 
@@ -43,44 +43,48 @@ export default function ReaderView(props: {
                 />
             </Clickable>
             <div class="pt-4" />
-            <Show when={pivotedPost().content.title}>{pt => <>
-                <span role="heading" class="block text-4xl font-black">
-                    {pt.text}
-                </span>
-                <div class="pt-4" />
-            </>}</Show>
-            <Show when={pivotedPost().content.author}>{author => <>
-                <Show if={
-                    settings.authorPfp() === "on"
-                } when={author.pfp} fallback={"By "}>{pfp => <>
-                    <AuthorPfp pfp={pfp} />{" "}
+            <Show when={pivotedPost().content} fallback={<>
+                pivotedpost content is undefined
+            </>}>{content => <>
+                <Show when={content.title}>{pt => <>
+                    <span role="heading" class="block text-4xl font-black">
+                        {pt.text}
+                    </span>
+                    <div class="pt-4" />
                 </>}</Show>
-                <UserLink
-                    client_id={author.client_id}
-                    href={author.link}
-                    color_hash={author.color_hash}
-                >
-                    {author.name}
-                </UserLink>{" "}
-            </>}</Show>
-            <Show when={pivotedPost().content.author}>{author => <>
-                <Show when={author.flair}>{flair => <>
-                    <Flair flairs={flair} />{" "}
+                <Show when={content.author}>{author => <>
+                    <Show if={
+                        settings.authorPfp() === "on"
+                    } when={author.pfp} fallback={"By "}>{pfp => <>
+                        <AuthorPfp pfp={pfp} />{" "}
+                    </>}</Show>
+                    <UserLink
+                        client_id={author.client_id}
+                        href={author.link}
+                        color_hash={author.color_hash}
+                    >
+                        {author.name}
+                    </UserLink>{" "}
                 </>}</Show>
+                <Show when={content.author}>{author => <>
+                    <Show when={author.flair}>{flair => <>
+                        <Flair flairs={flair} />{" "}
+                    </>}</Show>
+                </>}</Show>
+                <Show when={content.info?.in}>{in_sr => <>
+                    {" in "}<LinkButton
+                        action={{url: in_sr.link, client_id: in_sr.client_id}}
+                        style="previewable"
+                    >{in_sr.name}</LinkButton>{" "}
+                </>}</Show>
+                <InfoBar post={content} opts={{
+                    client_id: pivotedPost().v.client_id,
+                    frame: pivotedPost().v,
+                    id: props.pivot,
+                    flat_frame: null,
+                }} />
+                <Body body={content.body} autoplay={true} />
             </>}</Show>
-            <Show when={pivotedPost().content.info?.in}>{in_sr => <>
-                {" in "}<LinkButton
-                    action={{url: in_sr.link, client_id: in_sr.client_id}}
-                    style="previewable"
-                >{in_sr.name}</LinkButton>{" "}
-            </>}</Show>
-            <InfoBar post={pivotedPost().content} opts={{
-                client_id: pivotedPost().v.client_id,
-                frame: pivotedPost().v,
-                id: props.pivot,
-                flat_frame: null,
-            }} />
-            <Body body={pivotedPost().content.body} autoplay={true} />
         </div>
     </div>;
 }
