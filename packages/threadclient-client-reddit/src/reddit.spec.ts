56b6bbaa7ad8ce306358ebf3f7155789e265cac1
pfg <pfg@pfg.pw>
2026-02-12 00:11:47 -0800

    rtj to richtext test

--- packages/threadclient-client-reddit/src/reddit.spec.ts
+++ packages/threadclient-client-reddit/src/reddit.spec.ts
@@ -5,29 +5,6 @@ import type * as Reddit from "api-types-reddit";
 import {rt} from "api-types-generic";
 import * as reddit from "./reddit";
 
-let expect_count = 0;
-let expect_pass_count = 0;
-function expect(expected: string, value: unknown) {
-    expect_count += 1;
-    let pfx = "\x1b[92m[✓]\x1b(B\x1b[m ";
-    let ems = "";
-    if(expected === "" + value) {
-        expect_pass_count += 1;
-    }else{
-        pfx = "\x1b[91m[✗]\x1b(B\x1b[m ";
-        ems = " ↳ `\x1b[97m"+expected+"\x1b(B\x1b[m` expected";
-    }
-    console.log(pfx + value);
-    if(ems) console.log(ems);
-}
-function exit() {
-    const all_passed = expect_count === expect_pass_count;
-    const color = all_passed ? "\x1b[92m" : "\x1b[91m";
-    console.log("");
-    console.log(color + expect_pass_count + "/" + expect_count + " tests passed.\x1b(B\x1b[m ");
-    process.exit(all_passed ? 0 : 1);
-}
-
 // === RTJ TO RICHTEXT ===
 
 function fmtRichtext(richtext: Generic.Richtext.Paragraph[]): string {
@@ -35,17 +12,22 @@ function fmtRichtext(richtext: Generic.Richtext.Paragraph[]): string {
 }
 function fmtRichtextPar(richtext: Generic.Richtext.Paragraph): string {
     if(richtext.kind === "paragraph") {
-        return richtext.children.map(fmtRichtextSpan).join("");
+        return fmtRichtextSpans(richtext.children);
     }else return "%TODO fmt: ["+richtext.kind+"]";
 }
 function fmtRichtextStyle(style: Generic.Richtext.Style): string {
     const res = (style.strong ?? false ? "*" : "") + (style.emphasis ?? false ? "_" : "") + (style.strikethrough ?? false ? "~" : "") + (style.superscript ?? false ? "^" : "");
     return res;
 }
+function fmtRichtextSpans(richtext: Generic.Richtext.Span[]): string {
+    return richtext.map(fmtRichtextSpan).join("");
+}
 function fmtRichtextSpan(richtext: Generic.Richtext.Span): string {
     if(richtext.kind === "text") {
         const styl = fmtRichtextStyle(richtext.styles);
         return "[" + styl + richtext.text + styl + "]";
+    } else if (richtext.kind === "link") {
+        return `[${fmtRichtextSpans(richtext.children)}](${richtext.url})`;
     }else return "[TODO span:"+richtext.kind+"]";
 }
 
@@ -54,9 +36,35 @@ function testsample(doc: Reddit.Richtext.Document) {
 }
 
 
-// https://thread.pfg.pw/#reddit/r/Solo_Roleplaying/comments/x99b3q/kult_rpg_solo_play_example/inqztdb/?context=3&sort=new
-expect(`
-[I almost got a solo game of ][_Kult_][ going, but I switched to ][_Silent Legions_][ instead. Although the Machine City in Metropolis seemed to bleed through into my game anyways, so...]
-`.trim(), testsample({"document":[{"c":[{"e":"text","t":"I almost got a solo game of Kult going, but I switched to Silent Legions instead. Although the Machine City in Metropolis seemed to bleed through into my game anyways, so...","f":[[2,28,4],[2,58,14]]}],"e":"par"}]}));
+test("richtext sample 1", () => {
+    // https://thread.pfg.pw/#reddit/r/Solo_Roleplaying/comments/x99b3q/kult_rpg_solo_play_example/inqztdb/?context=3&sort=new
+    expect(`
+    [I almost got a solo game of ][_Kult_][ going, but I switched to ][_Silent Legions_][ instead. Although the Machine City in Metropolis seemed to bleed through into my game anyways, so...]
+    `.trim(), testsample({"document":[{"c":[{"e":"text","t":"I almost got a solo game of Kult going, but I switched to Silent Legions instead. Although the Machine City in Metropolis seemed to bleed through into my game anyways, so...","f":[[2,28,4],[2,58,14]]}],"e":"par"}]}));
+});
+test("regression #10", () => {
+    // https://github.com/pfgithub/threadclient/issues/10
+    // https://thread.pfg.pw/#reddit/r/modnews/comments/14n9426/accessibility_updates_to_mod_tools_part_2/jq67gvk/?context=3&sort=confidence
+    expect(testsample({"document":[
+        {"c":[{"e":"text","t":"I just want to make sure more admins see many of the existing bugs that still exist in the moderator workflow on mobile. I posted this 4 days ago to Mod Support."}],"e":"par"},
+        {"c":[{"u":"https://www.reddit.com/r/ModSupport/comments/14jkqiy/this_is_the_current_experience_moderating_on/","e":"link","t":"This is the Current Experience Moderating on Mobile"}],"e":"par"},
+        {"c":[{"e":"text","t":"I want the admins to understand fully, that while it's technically possible to most things on mobile these days why mods don't want to. The issues with your app simply don't exist with third party developers that we are used to using, and some of what your app still lacks, they, and other tools have had for years. When the first party app is too tedious and frustrating that it makes you quit and open your third party app, you're simply not ready to close that door.","f":[[2,121,13]]}],"e":"par"},
+        {"c":[{"e":"text","t":"I'm appreciative of the changes (many of which go back more than a year). But you are seriously not ready on this timeline."}],"e":"par"},
+        {"c":[{"e":"text","t":"I will simply be moderating less, and at best, increasing the amount of response time, not just in efficiency but also in that I'll just wait until I can get to a desktop so I won't have to put up with the tedium and frustrations of the mod queue on mobile."}],"e":"par"},
+        {"c":[{"e":"text","t":"Why can't I preview markdown formatting before submitting a response? come on. Your own mobile mod queue doesn't even support markdown...","f":[[2,70,7]]}],"e":"par"},
+    ]}))
+        .toMatchInlineSnapshot(`
+          "[I just want to make sure more admins see many of the existing bugs that still exist in the moderator workflow on mobile. I posted this 4 days ago to Mod Support.]
 
-exit();
+          [[This is the Current Experience Moderating on Mobile]](https://www.reddit.com/r/ModSupport/comments/14jkqiy/this_is_the_current_experience_moderating_on/)
+
+          [I want the admins to understand fully, that while it's technically possible to most things on mobile these days why mods ][_don't want to_][. The issues with your app simply don't exist with third party developers that we are used to using, and some of what your app still lacks, they, and other tools have had for years. When the first party app is too tedious and frustrating that it makes you quit and open your third party app, you're simply not ready to close that door.]
+
+          [I'm appreciative of the changes (many of which go back more than a year). But you are seriously not ready on this timeline.]
+
+          [I will simply be moderating less, and at best, increasing the amount of response time, not just in efficiency but also in that I'll just wait until I can get to a desktop so I won't have to put up with the tedium and frustrations of the mod queue on mobile.]
+
+          [Why can't I preview markdown formatting before submitting a response? ][_come on_][. Your own mobile mod queue doesn't even support markdown...]"
+        `);
+});
+
