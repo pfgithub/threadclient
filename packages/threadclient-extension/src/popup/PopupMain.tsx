d60978aa80772e05e5887b0891f949a72d5d5fd0
pfg <pfg@pfg.pw>
2026-05-03 15:11:29 -0700

    popup wip

--- packages/threadclient-extension/src/popup/PopupMain.tsx
+++ packages/threadclient-extension/src/popup/PopupMain.tsx
@@ -1,17 +1,31 @@
 import {JSX} from "solid-js";
 import {render} from "solid-js/web";
+import { resolveThreadClientSupportedURL } from "tmeta-util";
+import { Show } from "tmeta-util-solid";
 import browser from "webextension-polyfill";
 
-function Popup(): JSX.Element {
+function Popup(props: {tab?: browser.Tabs.Tab}): JSX.Element {
+    const supported = resolveThreadClientSupportedURL(props.tab?.url ?? "");
     return <div>
+        <Show if={supported != null} fallback={<>
+            <a href={`https://thread.pfg.pw/#shell/find-threads?u=${encodeURIComponent(props.tab?.url ?? "")}`}>
+                Search for Threads
+            </a>
+        </>}>
+            <a href={`https://thread.pfg.pw/#${props.tab?.url}`}>
+                Open in ThreadClient
+            </a>
+        </Show>
         <button onClick={() => {
             void browser.runtime.openOptionsPage();
-        }}>open options</button>
-        <a href="error" target="_blank" rel="noopener">
-            [if page == reddit]Open in ThreadClient,
-            [if page == threadclient]Open in Reddit,
-        </a>
+            window.close();
+        }}>Options</button>
     </div>;
 }
 
-render(() => <Popup />, document.getElementById("main") ?? document.body);
+browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
+    render(() => <Popup tab={tabs[0]} />, document.getElementById("main") ?? document.body);
+}).catch(e => {
+    console.error(e);
+    document.body.appendChild(document.createTextNode(e.toString()));
+});
